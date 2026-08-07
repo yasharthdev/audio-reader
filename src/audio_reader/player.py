e09f860e8e8f46d283c4b39d389e3f95beb9c52d
yasharthdev <yasharth1798@gmail.com>
2026-08-07 23:59:57 +0530

    Use edge-tts for text to speech

--- src/audio_reader/player.py
+++ src/audio_reader/player.py
@@ -1,7 +1,18 @@
-import pyttsx3
+import edge_tts
+import subprocess
+import asyncio
+from pathlib import Path
 
 def speak_paragraph(text: str) -> None:
-    engine = pyttsx3.init()
-    # speak the text out loud
-    engine.say(text)
-    engine.runAndWait()
+    # calling the async helper function
+    asyncio.run(_generate_audio(text, "temp.mp3"))
+    # playing temp.mp3 with afplay (native macOS audio player command)
+    subprocess.run(["afplay", "temp.mp3"])
+    # deleting the temp.mp3 file to save space
+    Path("temp.mp3").unlink()
+
+
+async def _generate_audio(text: str, file_path: str) -> None:
+    # edge-tts requires a voice string. "en-US-AriaNeural" is a great default.
+    communicate = edge_tts.Communicate(text, "en-US-AndrewNeural")
+    await communicate.save(file_path)
