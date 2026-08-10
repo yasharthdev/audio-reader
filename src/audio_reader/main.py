069cc592894a478bf733f7b62400f4ff61b05961
yasharthdev <yasharth1798@gmail.com>
2026-08-10 13:11:13 +0530

    refactor: encapsulate streaming engine and add global listener toggle

--- src/audio_reader/main.py
+++ src/audio_reader/main.py
@@ -1,12 +1,8 @@
 from audio_reader.reader import load_paragraphs, list_available_books, list_avaiable_voices
 from audio_reader.player import download_audio, play_audio, is_playing_audio, cleanup_audio, cleanup_all_audio
-from pynput import keyboard
+from audio_reader.engine import stream_audiobook
 import argparse
 import sys
-import pygame
-import subprocess
-import threading
-import queue
 
 def main():
     # initializing the parser
@@ -54,101 +50,10 @@ def main():
         list_avaiable_voices()
         sys.exit(0)
     elif args.filepath:
+        # load the paragraphs from the audiobook
         paragraphs = load_paragraphs(args.filepath)
-
-        
-        # pynput automatically passes a key here, whenever pressed by the user
-        def on_press(key):
-            try:
-                # key.char gives the actual letter pressed (like 'q' or 's')
-                if key.char == "q":
-                    playback_state["quit"] = True
-                elif key.char == "s":
-                    playback_state["skip"] = True
-            except AttributeError:
-                # this is the block where keys like shift, enter, etc are sent
-                if key == keyboard.Key.space:
-                    # if pause if False, it changes to True and vice versa
-                    playback_state["pause"] = not(playback_state["pause"])
-
-
-        # start the listener once in the background
-        listener = keyboard.Listener(on_press=on_press)
-        # start() runs in the background without freezing python
-        listener.start()
-
-        # the tray that holds exactly five files
-        audio_queue = queue.Queue(maxsize=5)
-
-        def background_downloader(paragraphs: list[str], voice: str, speed: str) -> None:
-            """Download 5 paras, put them in audio_queue"""
-            for i, para in enumerate(paragraphs):
-                filepath = f"temp_{i}.mp3"
-                # download the file
-                download_audio(para, filepath, voice, speed)
-                # Put it on the tray (if the tray is full, it waits automatically)
-                audio_queue.put(filepath)
-
-        # run the downloader thread in the background
-        threading.Thread(
-            target=background_downloader,
-            args=(paragraphs, args.voice, args.speed),
-            daemon=True
-        ).start()
-        
-        for i, para in enumerate(paragraphs):
-            playback_state = {"quit": False, "skip": False, "pause": False}
-
-            # to clear the terminal before printing any paragraphs
-            subprocess.run(["clear"])            
-            print(para)
-
-            try:
-                audio_file = audio_queue.get()
-                play_audio(audio_file)
-
-                was_paused = False
-
-                while True:
-                    # check for pause
-                    if playback_state["pause"] and not was_paused:
-                        pygame.mixer.music.pause()
-                        was_paused = True
-                    # check if we unpaused
-                    elif not playback_state["pause"] and was_paused:
-                        pygame.mixer.music.unpause()
-                        was_paused = False
-                        # give pygame 50ms to wake up before checking get_busy()
-                        pygame.time.wait(50)
-                    # if we aren't paused and the audio isn't playing, break the loop
-                    if not playback_state["pause"] and not is_playing_audio():
-                        break
-                    
-                    # check for skip
-                    if playback_state["skip"]:
-                        pygame.mixer.music.stop()
-                        # exit the while loop and continue to next para
-                        break
-                    # check for quit
-                    if playback_state["quit"]:
-                        pygame.mixer.music.stop()
-                        # exit the while loop **
-                        break
-                    pygame.time.Clock().tick(10)            
-
-            # clean up the audio every time, even in accidental shutdown cases 
-            finally:
-                cleanup_audio(audio_file)
-
-            # ** break the for loop to completely quit the program
-            if playback_state["quit"]:
-                break
-
-        # clean up the background listener after we're done
-        listener.stop()
-
-        # delete any leftover downloaded files by the downloader
-        cleanup_all_audio()
+        # stream the audiobook
+        stream_audiobook(paragraphs, args.voice, args.speed)
     else:
         print("Please provide book_name.txt or use --list to see available books")
         sys.exit(1)
